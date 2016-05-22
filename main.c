465732159b28504985ef8b9275ade00a249b7f34
Gerwert <gerwert@example.com>
2016-05-22 10:38:18 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,37 @@
+/*
+ *  AnyBar cli
+ *
+ *  Copyright © 2016, G. Stevens. All rights reserved.
+ */
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main(int argc, const char * argv[]) {    
+    printf("Sending AnyBar command...\n");
+    
+    int clientSocket;
+    const char* message = argv[1];
+    struct sockaddr_in serverAddr;
+    socklen_t addr_size;
+    
+    /* Create UDP socket */
+    clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+    
+    /* Configure server settings in serverAddr struct */
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(1738);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    
+    /* Initialize size variable */
+    addr_size = sizeof serverAddr;
+    
+    /* Send UDP message to AnyBar */
+    sendto(clientSocket, message, strlen(message),0,(struct sockaddr *)&serverAddr,addr_size);
+    
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+#README
+
+A minimal C command line client for [AnyBar](https://github.com/tonsky/AnyBar).
+
+## Usage
+
+    $ anybar-cli red    
+    $ anybar-cli green    
+    $ anybar-cli blue    
+    $ anybar-cli exclamation
